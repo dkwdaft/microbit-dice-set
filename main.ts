3f5b007d15cc36d464362cde7f65900e28342661
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2025-07-20 15:55:22 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,43 @@
-function d8() {
+function d8 () {
     basic.showNumber(randint(1, 8))
 }
-
-function d100() {
+input.onButtonPressed(Button.A, function () {
+	
+})
+function d100 () {
     basic.showNumber(randint(1, 100))
 }
-
-function d6() {
+function d6 () {
     basic.showNumber(randint(1, 6))
 }
-
-function d4() {
+function d4 () {
     basic.showNumber(randint(1, 4))
 }
-
-function d10() {
+input.onButtonPressed(Button.B, function () {
+	
+})
+function d10 () {
     basic.showNumber(randint(1, 10))
 }
-
-function d20() {
+input.onGesture(Gesture.Shake, function () {
+	
+})
+function d20 () {
     basic.showNumber(randint(1, 20))
 }
-
-function d12() {
+function logic () {
+	
+}
+function d12 () {
     basic.showNumber(randint(1, 12))
 }
-
-function logic() {
-    
-}
-
 let choice = 0
-let options = [4, 6, 8, 10, 12, 20, 100]
+let options = [
+4,
+6,
+8,
+10,
+12,
+20,
+100
+]
