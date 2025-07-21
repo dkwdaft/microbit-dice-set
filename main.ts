95afec2ad40afaeaaeb8314a81c7834c4a16deed
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2025-07-21 09:02:06 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,44 +1,44 @@
-function d8 () {
+function d8() {
     basic.showNumber(randint(1, 8))
 }
-input.onButtonPressed(Button.A, function () {
-	
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
 })
-function d100 () {
+function d100() {
     basic.showNumber(randint(1, 100))
 }
-function d6 () {
+
+function d6() {
     basic.showNumber(randint(1, 6))
 }
-function d4 () {
+
+function d4() {
     basic.showNumber(randint(1, 4))
 }
-input.onButtonPressed(Button.B, function () {
-	
+
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
 })
-function d10 () {
+function d10() {
     basic.showNumber(randint(1, 10))
 }
-input.onGesture(Gesture.Shake, function () {
-	
+
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
 })
-function d20 () {
+function d20() {
     basic.showNumber(randint(1, 20))
 }
-function logic () {
-	
+
+function logic() {
+    
 }
-function d12 () {
+
+function d12() {
     basic.showNumber(randint(1, 12))
 }
+
 let choice = 0
-let options = [
-4,
-6,
-8,
-10,
-12,
-20,
-100
-]
-basic.showString("D" + options[choice])
+let options = [4, 6, 8, 10, 12, 20, 100]
+basic.showString("D" + ("" + options[choice]))
